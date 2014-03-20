dc554f2638892f23e0d613be2d0babf55171903b
Sami Purmonen <purmonen@kth.se>
2014-03-20 18:38:53 +0100

    Major refactor

--- js/services.js
+++ js/services.js
@@ -7,7 +7,58 @@
 // In this case it is a simple value service.
 var services = angular.module('myApp.services', ['ngResource', 'ngCookies']);
 
-services.factory('Post', ['$resource', '$http',
-  function($resource, $http) {
-    return $resource('http://localhost:3000/:user/posts/KTH', {user: 'root'});
+/*
+services.factory('Post', ['$resource',
+  function($resource) {
+    return $resource('http://localhost:3000/:user/KTH', {user: 'root'});
   }]);
+*/
+
+services.factory('Post', ['$resource',
+	function($resource) {
+		var data = [];
+		var $res = $resource('http://localhost:3000/:user/KTH', {user: 'root'});
+		var changeManager = new ChangeManager([]);
+
+		$res.query(function(posts) {
+			changeManager = new ChangeManager(posts);
+		});
+
+		return {
+			add: function(post) { 
+				changeManager.change(function(posts) {
+					posts.push(post);
+				});
+			},
+			edit: function(editPost, index) {
+				changeManager.change(function(posts) {
+					var post = posts[index];
+					post.content = editPost.content;
+					post.color = editPost.color;
+				});
+			},
+			delete: function(index) {
+				changeManager.change(function(posts) {
+					posts.splice(index, 1);
+				});
+			},
+			get: function() { 
+				return changeManager.get();
+			},
+			save: function(success, error) {
+				$res.save(changeManager.get()).$promise.then(success, error);
+			},
+			canUndo: function() {
+				return changeManager.canUndo();
+			},
+			canRedo: function() {
+				return changeManager.canRedo();
+			},
+			undo: function() {
+				changeManager.undo();
+			},
+			redo: function() {
+				changeManager.redo();
+			}
+		};
+	}]);
